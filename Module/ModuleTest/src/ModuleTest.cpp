46b61d00ffa0766f1327d58023eb371c6265d52e
IrvingLu <irvinglu@reallusion.com>
2019-01-22 20:48:28 +0800

    RL unit test template 1st commit

---
+++ Module/ModuleTest/src/ModuleTest.cpp
@@ -0,0 +1,28 @@
+// Module test code
+
+#include <gmock/gmock.h>
+
+#include "Module/Module.h"
+
+using namespace testing;
+
+// Classic Assertion:
+//   Fatal assertion          Nonfatal assertion      Verifies
+// ASSERT_EQ(val1, val2);   EXPECT_EQ(val1, val2);   val1 == val2
+// ASSERT_NE(val1, val2);   EXPECT_NE(val1, val2);   val1 != val2
+// ASSERT_LT(val1, val2);   EXPECT_LT(val1, val2);   val1 < val2
+// ASSERT_LE(val1, val2);   EXPECT_LE(val1, val2);   val1 <= val2
+// ASSERT_GT(val1, val2);   EXPECT_GT(val1, val2);   val1 > val2
+// ASSERT_GE(val1, val2);   EXPECT_GE(val1, val2);   val1 >= val2
+// 
+// Test Case:
+//   TEST( TestSuiteName, TestName ) {...}
+//
+// or using test fixture:
+//   class TestSuiteName : public Test
+//   {
+//   protected:
+//        virtual void SetUp() {}
+// 	      CModule	m_kModule;    // your test module
+//   };
+//   TEST_F( TestSuiteName, TestName ) {...}
